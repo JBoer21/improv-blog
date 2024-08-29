4478423cb4d3f38d3fe4be82d3eccd9570f5d067
Jehan <boerjehan@gmail.com>
2024-08-29 16:15:29 -0400

    changed header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,52 +1,49 @@
 import type { MetaFunction } from "@remix-run/node";
-import { TestShadcnDialog } from "app/components/custom/mycomponent";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "app/components/ui/hover-card";
+import { Button } from "app/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Jehan's Improv Journey" },
+    { name: "description", content: "Follow Jehan&apos;s journey in improv" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <div className="mt-6">
-        <TestShadcnDialog />
-      </div>
+    <div className="font-sans p-4 flex flex-col items-center">
+      <h1 className="text-6xl font-bold text-black mb-8 p-6">
+        Jehan&apos;s Improv Journey
+      </h1>
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button variant="outline">Blog Posts</Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-64">
+          <ul className="space-y-2">
+            <li>
+              <a href="/post1" className="text-blue-600 hover:underline">
+                My First Improv Class
+              </a>
+            </li>
+            <li>
+              <a href="/post2" className="text-blue-600 hover:underline">
+                Learning to &quot;Yes, And&quot;
+              </a>
+            </li>
+            <li>
+              <a href="/post3" className="text-blue-600 hover:underline">
+                Performing in My First Show
+              </a>
+            </li>
+            {/* Add more blog post links as needed */}
+          </ul>
+        </HoverCardContent>
+      </HoverCard>
     </div>
   );
 }
