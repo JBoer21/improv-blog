7ea6b2299329cebb33d912df4e11d27c3c5ce4db
Jehan <boerjehan@gmail.com>
2024-10-27 14:06:10 -0400

    added the paragraphs for blog 3

---
+++ app/data/blog3/blog3.ts
@@ -0,0 +1,35 @@
+const par1 = `This weekend, I had the genuine pleasure of watching an improv show at Baltimore Improv Group. It was great and probably worth the 10$ I spent to get in. Allow me to share my observations.`;
+
+const par2 = `The show was a little bit different from what we have seen in class, as all the performances came in the form of monologues, something we haven't seen too much of yet. There were 6 performances, each performer monologuing as some very interesting character. My personal favorite character was someone acting as a farmer in the universe where the Wizard of Oz takes place. He stood before a court and begged for help as his farm had been pillaged by some of the magic creatures of the land.`;
+
+const par3 = `In my humble opinion, his funniest line was: "I can't go out into my crop without having shit thrown at me by the flying monkeys. I've had pink eye for 5 months!".`;
+
+const par4 = `Each character got on stage and for around 10 minutes, monologued. It's difficult to state for sure whether a monologue is improvised or not (as you aren't going off other people's offers) but regardless, each monologue was good fun and reacted to the crowd in different ways. Then, at the end, we got to see all the characters interact in a group panel where we got to ask the characters questions. While difficult to state whether the collective group exhibited group mind (due to the fact that most performances were individual), an evaluation on group mind can still be done. Group mind was certainly exhibited in this performance in many ways. For one, when characters were monologuing, other characters would join the crowd to interact with them in minor ways. Supportive energy and the contribution to the show's atmosphere through this type of action were great exhibitions of group mind.`;
+
+const par5 = `Additionally, all characters were really playing at the top of their intelligence. When the audience was asking interview style questions, they answered exactly as their characters would have. One of the characters was the Boogey Man, who had grown up and instead of scaring the little girl under the bed, he gave her financial life advice. I asked him what my financial goals should be for my 20s, to which he replied: "If I answered I would be out of a job. Go out and spend, spend, spend honey!". All these elements combined well enough to establish that the ensemble exhibited group think.`;
+
+const par6 = `While the show was a great laugh and I thought the ensemble did fantastic, there are some critiques to the show. In some moments, it did not really feel like improv. For example, one character, while hilarious, was reading off of a script. There were other smaller moments like this, where it was obvious choreography and scripting were at play. I argue that the unscripted nature of improv is what makes it beautiful and fun. By scripting, you lose a ton of key elements. Regardless, there is no real formula to funny, and despite the lack of improvision, the performance was fantastic.`;
+
+const par7 = `It's fair to say that that the show inspired me in some ways. To start, it inspired me to be more creative. The characters being played in the show were so unique and funny it made me jealous that I wouldn't have been able to come up with such hilarity. I think going forward, I want to be less safe and emulate much more unique and 'dangerous' characters.`;
+
+const par8 = `Another form of inspiration I drew from was how to heighten a lot better. I think a very common theme we see in class, and I have certainly fallen hand to is to ask questions in order to string the scene along. They are so easy to ask and therefore are the easy option to get out of a tight scene. However, they don't do much, as we see quite often. Seeing monologues, we find performers are unable to answer questions, and therefore have to escape tight spaces with more creative methods, essentially just making stuff up to keep chatting about. While it may seem elementary, this method works perfectly for group work as well. As I was watching the show, I was thinking to myself about how instead of asking a question, just assume the answer of said question and state it. While this method may seem elementary, it's not as obvious or easy as it sounds, but certainly acts as a fantastic way to heighten a scene. I hope to use this inspiration in class.`;
+
+const par9 = `These two insights can easily be applied to both my improv journey and my final performance.`;
+
+const par10 = `In the case of my improv journey, I think finding better ways to heighten will certainly benefit me. I think by finding better ways to move along a scene other than asking questions, the flow, fluidity and energy of said scene will improv. By implementing the method of simply answering the questions I would have asked otherwise, group think is a lot stronger and scenes feel a lot more real.` 
+
+const par11 = `In the case of my final performance, I think thinking of and emulating unique characters is a strategy that will benefit everyone. By being a less basic character, there is not only room for more hilarity, but more to draw on and heighten from, to the benefit of both the audience and the ensemble.`;
+
+export const blog2part1 = [
+    par1,
+    par2,
+    par3,
+    par4,
+    par5,
+    par6,
+    par7,
+    par8,
+    par9,
+    par10,
+    par11
+  ];
