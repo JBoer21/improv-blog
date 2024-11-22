82403e22664c597a91cb825ac30f2667f4c60771
Jehan <boerjehan@gmail.com>
2024-11-22 11:00:01 -0500

    added blog4 paragraphs

---
+++ app/data/blog4/blog4.ts
@@ -0,0 +1,20 @@
+const par1 = `I believe we have been given the opportunity to reflect on the final performance, so I am going to do that while still answering some of the "What can you change" questions. The final performance was this morning, and it felt fantastic.`;
+
+const par2 = `Last night, I dreamt of the improv show. I dreamt of the improv show, and how it would go. I saw the other team go, I saw my own team go, and I saw myself go. I was pretty locked in and inspired and created a good image in my head of what it might look like. The actual performance was better. It was fluid, funny, and overall, it just felt supernatural. I can tell that I, as well as my peers, have grown so much. We have improved in our improvisation and overall just gotten much more comfortable. I am completely happy with the way things went. That being said, that doesn’t mean it could not have been better.`;
+
+const par3 = `What do I want to change? I would be lying if I didn’t feel a little bit of a competitive spirit when comparing myself to the other group. I think they executed their short form improv terrifically. While I share the same sentiment with our first game, Change, I do think that our second game lacked a bit. I really liked team No Butts’ dialogue style game, “And that’s how I died.” I think giving some sort of context in terms of 1) a starting idea and 2) a goal to achieve (how you died) adds some clarity to how the improv works. Additionally, it’s all independent of what others do. You run by your own speed. Looking back, our dialogue-focused game, Objection, felt a little awkward. We could have done a better job expanding, but also, the style of the game just lacks some of the elements that really make a game enticing to an audience. That’s how I died really gives the individual some creative liberty.`;
+
+const par4 = `On the other hand, our first short form, Change, went great. I really think this game and the 2-3 person scenes capture the essence of true improv. With long form, it gets difficult to keep up, and short form allowed us to show our ability to establish a reality and then expand on it immediately. Shoutout Jayden, because he was the MVP of our show. It was a genuine pleasure to do this scene with him. He, Christine, and I did the ocean scene, and I think it really allowed us to showcase the skills we have developed over the past two months. We quickly established our three Ws, and then expanded almost automatically. Christine did fantastic with the work in coming off stage, and I think we did a good job coming up with new things to work on. Super proud of this scene.`;
+
+const par5 = `Lastly, our longform. If I am being honest, longform has probably been the most challenging. It feels like being put in a bubble with six other people and constantly trying to push on the edges of the bubble to expand it. The hardest part is that you have to incorporate the expansions that other people add as well. Somehow, my group managed to end up at a small-town, parent-teacher conference-esque scene for the second time. I guess it's habitual. I think, given the challenges of such a scene, we did fantastically. I love how the norm has become to all converge into one massive scene where everyone is on stage. I am proud of my team.`;
+
+const par6 = `Overall, the show went well. The experience has been great, and I think my team can really hold our heads high on this one.`;
+
+export const blog4 = [
+    par1,
+    par2,
+    par3,
+    par4,
+    par5,
+    par6
+]
